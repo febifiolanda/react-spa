326aa0b14c450de3e9dd734e6af244ab948f800a
febi fiolanda <febyfiolanda@gmail.com>
2019-05-07 09:00:29 +0700

    react spa

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
+import Main from './component/page/Main';
+
+
+class App extends Component {
+    render() {
+        return (
+            <div>
+                <Link to='/home'>Home</Link> &nbsp; &nbsp;
+                <Link to='/profile'>Profile</Link> &nbsp; &nbsp;
+                <Link to='/portofolio'>Portofolio</Link> &nbsp; &nbsp;
+                <Link to='/contact'>Contact</Link> &nbsp; &nbsp;
+                <hr/>
+                <Main/>
+            </div> 
+
+        );
+    }
+}
+
+export default App;
